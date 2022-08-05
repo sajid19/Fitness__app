0523fc26118f9e507436c6872614508231e49acd
sajid_f57ovgd <sajidhasan2641@gmail.com>
2022-08-05 12:29:27 -0700

    k

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -6,6 +6,32 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({exercises , setExercises , bodyPart}) => {
+  const [currentPage , setCurrentPage] = useState(1);
+  const exercisePerPage = 9;
+  const indexLastExercises  = currentPage * exercisePerPage;
+  const indexFirstExercise = indexLastExercises - exercisePerPage;
+  const currentExercise = exercises.slice(indexFirstExercise , indexLastExercises)
+
+
+
+  const paginate = (e , value) => {
+    setCurrentPage(value)
+    window.scrollTo({top:'1800' , behavior : 'smooth'})
+  }
+  useEffect(()=> {
+    const fetchExercisesData = async () => {
+      let exercisesData = []
+      if (bodyPart === 'all'){
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises',exerciseOptions);
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+        exerciseOptions);
+      }
+      setExercises(exercisesData);
+    }
+    fetchExercisesData();
+
+  }, [bodyPart])
   
   return (
     <Box
@@ -15,9 +41,22 @@ const Exercises = ({exercises , setExercises , bodyPart}) => {
     <Typography variant='h3' mb= '46px'>Showing Result</Typography>
     <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' }}} flexWrap ='wrap'
      justifyContent='center'>
-      {exercises.map((exercise , index) =>(
+      {currentExercise.map((exercise , index) =>(
         <ExerciseCard key={index} exercise={exercise}/>
       ))}
+    </Stack>
+    <Stack mt= "100px" alignItems= "center">
+     {exercises.length > 9 && (
+      <Pagination
+      color= 'standard'
+      shape='rounded'
+      defaultPage={1}
+      count = {Math.ceil(exercises.length /exercisePerPage)}
+      page= {currentPage}
+      onChange={paginate}
+      size ='large'
+     />)}
+
     </Stack>
 
     </Box>
